b0967c77ea83eadfc2fd743448020d4784d78def
cocreatorr <stelvimmxxi@gmail.com>
2026-01-18 21:09:41 +0700

    Add blockContent schema

---
+++ stepwise-studio/schemas/blockContent.ts
@@ -0,0 +1,48 @@
+import { defineType, defineField } from "sanity";
+
+export default defineType({
+  name: "blockContent",
+  title: "Block Content",
+  type: "array",
+  of: [
+    defineField({
+      type: "block",
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "Heading 1", value: "h1" },
+        { title: "Heading 2", value: "h2" },
+        { title: "Heading 3", value: "h3" },
+        { title: "Quote", value: "blockquote" },
+      ],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
+      ],
+      marks: {
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+          { title: "Code", value: "code" },
+        ],
+        annotations: [
+          {
+            name: "link",
+            type: "object",
+            title: "Link",
+            fields: [
+              {
+                name: "href",
+                type: "url",
+                title: "URL",
+              },
+            ],
+          },
+        ],
+      },
+    }),
+    defineField({
+      type: "image",
+      options: { hotspot: true },
+    }),
+  ],
+});
