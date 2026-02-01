1316f2c21d3e570a16a4f0c8c4757aaf0ae2a0cf
cocreatorr <stelvimmxxi@gmail.com>
2026-02-01 14:12:32 +0700

    Fix CI workflow paths and NextImage alt prop duplication

--- stepwise-web/lib/NextImage.tsx
+++ stepwise-web/lib/NextImage.tsx
@@ -1,4 +1,3 @@
-// lib/NextImage.tsx
 import Image, { ImageProps } from "next/image";
 import { urlFor, urlForBlur } from "./urlFor";
 
@@ -8,16 +7,22 @@ interface NextImageProps extends Omit<ImageProps, "src"> {
   height?: number;
 }
 
-export default function NextImage({ source, width = 800, height = 450, ...props }: NextImageProps) {
+export default function NextImage({
+  source,
+  width = 800,
+  height = 450,
+  alt = "",
+  ...rest
+}: NextImageProps) {
   return (
     <Image
       src={urlFor(source).width(width).url()}
-      alt={props.alt || ""}
+      alt={alt}
       width={width}
       height={height}
       placeholder="blur"
       blurDataURL={urlForBlur(source)}
-      {...props}
+      {...rest} // safe: no duplicate alt
     />
   );
 }
